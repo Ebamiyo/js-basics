dd37243d26d804b101d412269be9c08af73e92a6
Ebamiyo Omolade <ebamiyoomolade@gmail.com>
2023-02-09 16:23:01 +0100

    submit javaScript Assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function multiplyTwoNumbers(num1, num2) {
+    return num1 * num2
+}
+
+console.log(multiplyTwoNumbers(3, 4));
+
+
+let firstName = "Ebamiyo"
+let lastName =  "Omolade"
+let stuternTrack = "Backend Development"
+let skills = "Creativity, HTML and CSS"
+let favouriteColor = "White"
+let yearOfCountryIndependence = "1960"
+
+let aboutMe = "I am" + " " + firstName + " " + lastName + "," + " " + "my favourite color is" + " " + favouriteColor + "." + " " + "I study" + " " + stuternTrack + " " + "at Stutern." + " " + "My skills are" + " " + skills +  "." + " " + "My country Nigeria, gained her independence in" + " " + yearOfCountryIndependence + "."
+console.log(aboutMe)
